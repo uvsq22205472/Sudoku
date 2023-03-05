91019b745e2b7b6b4495aad050f9aa1534d56c37
probablyzora <miss.lunar32@gmail.com>
2023-03-05 16:13:13 +0100

    MàJ de sudoku.py.

--- sudoku.py
+++ sudoku.py
@@ -5,3 +5,19 @@
 ###        -------------       ###
 ###        -------------       ###
 ##################################
+
+from tkinter import *
+from random import randint
+
+############[Tunable]############
+global Canvas_Height
+global Canvas_Width
+Canvas_Width = 500
+Canvas_Height = 500
+#################################
+
+# Initialisation du tableau du jeu
+root = Tk()
+root.title("SUDOKU")
+Sudoku_Canvas = Canvas(root, width= Canvas_Width, height= Canvas_Height)
+
