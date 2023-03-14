dbb02d397605fa2fafc10bcb43177b2f07c368dd
probablyzora <oliwier.szmyt@ens.uvsq.fr>
2023-03-14 10:55:33 +0100

    Limitation de longeur/largeur de le l'écran

--- sudoku.py
+++ sudoku.py
@@ -18,16 +18,18 @@ Canvas_Height = 500
 ###### Initialisation du tableau du jeu
 root = Tk()
 root.title("SUDOKU")
+# 
+root.geometry("700x500")
+# => Fonction pour changer longeur/largeur de la fênetre trouvé après recherches sur
+####### https://www.tutorialspoint.com/how-to-place-an-image-into-a-frame-in-tkinter
 Sudoku_Canvas = Canvas(root, width= Canvas_Width, height= Canvas_Height)
-Sudoku_Canvas
-
 
 ###### Création du dictionnaire du jeu 
 ###### + liste des nombres qui ne peuvent pas êtres changés
 
 Sudoku_Dict = {}
 for CellNumber in range(1,82):
-    Sudoku_Dict[("Cell_"+str(CellNumber))] = None
+    Sudoku_Dict[("Cell_"+str(CellNumber))] = "X"
 print(Sudoku_Dict)
 
 Sudoku_RigidNumbers = []
