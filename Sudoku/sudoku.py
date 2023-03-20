fab67759d5d428121762ccdc4dfcb95e2e5d1a56
probablyzora <oliwier.szmyt@ens.uvsq.fr>
2023-03-20 15:17:45 +0100

    Débuts de la grille, mais c'est pas terminé.

--- Sudoku/sudoku.py
+++ Sudoku/sudoku.py
@@ -18,24 +18,55 @@ def InitializeBoard(Board_Root):
     Output:
     """
     return None
-def Create81Buttons(Board_Root):
-    """
-    """
-    for IterationNum in range(0,3):
+#def Create81Buttons(Board_Root):
+
+
         
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-global Canvas_Height
-global Canvas_Width
-Canvas_Width = 500
-Canvas_Height = 500
+Canvas_Width = 600
+Canvas_Height = 600
+Canvas_Padding = 20
+
+ThickLine_Color = "white"
+Line_Color = "gray"
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ###### Initialisation du tableau du jeu
 root = Tk()
-root.title("SUDOKU")
+root.title("SUDOKuU")
 root.geometry("800x600")
 # => Fonction pour changer longeur/largeur de la fênetre trouvé après recherches sur
 ####### https://www.tutorialspoint.com/how-to-place-an-image-into-a-frame-in-tkinter
-Sudoku_Canvas = Canvas(root, width= Canvas_Width, height= Canvas_Height)
+
+# Il y a une maniere plus interessante de faire ca avec une triple boucle for.
+
+
+
+
+
+Sudoku_Canvas = Canvas(root,bg="pink", width= Canvas_Width, height= Canvas_Height)
+Sudoku_Canvas.grid(column=Canvas_Padding,row=Canvas_Padding,columnspan=20,rowspan=20)
+
+
+for Y in range(0,4):
+    for X in range(0,4):
+        Sudoku_Canvas.create_rectangle((((Y-1)*200),((X-1)*200))
+
+#Sudoku_Canvas.create_rectangle((0,0),(200,200),width=2,outline=ThickLine_Color)
+#Sudoku_Canvas.create_rectangle((0,200),(200,400),width=2,outline=ThickLine_Color)
+#Sudoku_Canvas.create_rectangle((0,400),(200,600),width=2,outline=ThickLine_Color)
+
+#Sudoku_Canvas.create_rectangle((200,0),(400,200),width=2,outline=ThickLine_Color)
+#Sudoku_Canvas.create_rectangle((200,200),(400,400),width=2,outline=ThickLine_Color)
+#Sudoku_Canvas.create_rectangle((200,400),(400,600),width=2,outline=ThickLine_Color)
+
+#Sudoku_Canvas.create_rectangle((400,0),(600,200),width=2,outline=ThickLine_Color)
+#Sudoku_Canvas.create_rectangle((400,200),(600,400),width=2,outline=ThickLine_Color)
+#Sudoku_Canvas.create_rectangle((400,400),(600,600),width=2,outline=ThickLine_Color)
+#
+
+
+
+
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 ###### Création du dictionnaire du jeu 
@@ -45,9 +76,10 @@ Sudoku_Dict = {}
 for CellNumber in range(1,82):
     Sudoku_Dict[("Cell_"+str(CellNumber))] = "X"
 #print(Sudoku_Dict)
+#Creation de 
 
 Sudoku_RigidNumbers = []
-#test
+# Liste des nombres qui ne peuvent pas etre changes.
 
 
 
